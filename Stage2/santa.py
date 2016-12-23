2ca14e2e4d77e3d058f90ecb1cea0b5f205aeda3
Salman Shah <sbs.191197@gmail.com>
2016-12-23 21:47:34 +0530

    Stage 2 left pending

--- Stage2/santa.py
+++ Stage2/santa.py
@@ -1,3 +1,6 @@
+# n_name - Nickname
+# ch_n_name - Chosen NickName
+
 import csv
 import smtplib
 from email.mime.text import MIMEText
@@ -7,6 +10,7 @@ hash2 = {}
 hash3 = {}
 nicknames = []
 
+
 with open('Secret_Santa.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
 
@@ -15,32 +19,43 @@ with open('Secret_Santa.csv') as csvfile:
         nickname = row[2]
         email = row[3]
         hash1[nickname] = name
-        hash2[nickname] = email 
+        hash2[nickname] = email         
+        nicknames.append(nickname)
     
-    print hash1
+    #print hash1
+
 
 with open('Santa_Choose.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
 
     for row in readCSV:
         nickname = row[1]
-        chosen_nickname = row[2]
+        chosen_nickname = row[0]
         hash3[nickname] = chosen_nickname 
-        nicknames.append(nickname)
+
 
 for nickname in nicknames:
-    who = nickname
-    whatName = hash3[nickname]
+    #who = nickname
+    #whatName = hash3[nickname]
 
     #Mail written from here!
     title = 'Secret Santa'
-    msg_content = 'Hi this is a message to tell you about your Secret Santa\n' + hash1[whatName]
+    msg_content = """
+    Hey,
+    This is a message from the Secret Santa Team!<br><br>
+    Congratulations on being a part of IEEE-NITK Secret Santa 2k16.<br>
+    """ 
+    msg_content += "You are a Secret Santa for " + hash1[hash3[nickname]]
+    msg_content += """
+    <br>Hope to see you soon.
+    <br><br>Best regards,<br>
+    Secret Santa Team
+    """
     message = MIMEText(msg_content, 'html')
 
-    message['From'] = 'Sender Name <sender@server>'
-    message['To'] = 'Receiver Name <receiver@server>'
-    message['Cc'] = 'Receiver2 Name <receiver2@server>'
-    message['Subject'] = 'Secret Santa'
+    message['From'] = 'Sender <santa@ieee>'
+    message['To'] = 'Receiver <nitk@ieee>'
+    message['Subject'] = 'Secret Santa Disclosure'
 
     msg_full = message.as_string()
 
@@ -48,6 +63,6 @@ for nickname in nicknames:
     server.starttls()
     server.login('secret.santa.ieeenitk@gmail.com', 'ieeenitk2016')
     server.sendmail('secret.santa.ieeenitk@gmail.com',
-                    [ hash2[who] , 'salman.badshah@gmail.com'],
+                    [ hash2[nickname] , 'salman.badshah@gmail.com'],
                     msg_full)
     server.quit()
